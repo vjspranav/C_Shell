1c1bffd2aabfe9884c377c51ae21a198140a4c0d
vjspranav <pranavasri@live.in>
2020-10-02 09:10:00 +0530

    Try a diff version of pipe

--- commandHandler.h
+++ commandHandler.h
@@ -1,6 +1,6 @@
 #ifndef __COMMANDHANDLER_H
 #define __COMMANDHANDLER_H
 
-int runCommand(char **parsed, char* input);
+int runCommand(char **parsed, char* input, int in_des, int out_des);
 int runOwnCommand(int command_id, char* input, char** parsed);
 #endif
